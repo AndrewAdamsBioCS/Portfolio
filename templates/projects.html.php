51669ac7ee2bad4d5d0607edc3ceed43befe2e90
AndrewAdamsBioCS <awadams.bio.cs@gmail.com>
2023-11-29 16:24:54 -0500

    Updated project page

--- templates/projects.html.php
+++ templates/projects.html.php
@@ -1,5 +1,23 @@
+<div class="project-div">
+	<div class="project-description">
+	<h1><a href="https://www.russwwong.com/food-for-thought/" target="_blank">iPhone App: Food for Thought</a></h1>
+	<p>An interactive iOS mobile application designed to help users follow the practice of mindful eating, Food for Thought was collaboratively developed by a four-person team as part of a semester-long software engineering project. The project evolved in response to the needs of a real-life dietitian acting as our client, and spanned the full life cycle of development, from conceptualization to final product delivery.
+	</p>
+	<p>Visit our project GitHub repository <a href="https://github.com/royliver44/FoodForThought" target="_blank">here</a> to view the full application code.</p>
+	<p>
+	<strong>Skills</strong>: Swift, XCode, MVC, mobile app development, GitHub, Agile development
+	</p>
+	</div>
+
+	<div class="project-screenshot">
+		<a href="https://www.russwwong.com/food-for-thought/" target="_blank"><img src="images/food-for-thought.png" style="float:right;"></a>
+	</div>
+</div>
 
 <div class="project-div">
+	<div class="project-screenshot">
+		<a href="algPuzzleSolver.php"><img src="images/algPuzzleSolver-screenshot.png"></a>
+	</div>
 	<div class="project-description">
 	<h1><a href="algPuzzleSolver.php">Algorithmic Puzzle Solver</a></h1>
 	<p>Try your hand at the classic "Lights Out" puzzle, then see the performance of several search algorithms in solving it!</p>
@@ -8,17 +26,9 @@
 	<strong>Skills</strong>: Python, PyScript, search algorithms (DFS, BFS, A*), JavaScript, HTML, CSS
 	</p>
 	</div>
-
-	<div class="project-screenshot">
-		<a href="algPuzzleSolver.php"><img src="images/algPuzzleSolver-screenshot.png" style="float:right;"></a>
-	</div>
 </div>
 
 <div class="project-div">
-	<div class="project-screenshot">
-		<a href="https://awadams-bio-cs.shinyapps.io/DataAnalysisApp/" target="_blank"><img src="images/data-analysis-screenshot.png"></a>
-	</div>
-
 	<div class="project-description">
 	<h1><a href="https://awadams-bio-cs.shinyapps.io/DataAnalysisApp/" target="_blank">Statistical Analysis App</a></h1>
 	<p>An interactive app for performing basic statistical analysis of data sets. View summary statistics and generate plots and regression analysis, and save custom plots into a downloadable package.</p>
@@ -27,17 +37,31 @@
 	<strong>Skills</strong>: R, Shiny, HTML, CSS, data analysis, statistics
 	</p>
 	</div>
+	<div class="project-screenshot">
+		<a href="https://awadams-bio-cs.shinyapps.io/DataAnalysisApp/" target="_blank"><img src="images/data-analysis-screenshot.png" style="float:right;"></a>
+	</div>
 </div>
 
 <div class="project-div">
+<div class="project-screenshot">
+		<a href="invaders.php"><img src="images/game-screenshot.png"></a>
+	</div>
 	<div class="project-description">
 		<h1><a href="invaders.php">Retro Game</a></h1>
 		<p>	Aghhh! You're being attacked by monsters! Satiate them with cookies before they can get to you! But be careful: you only have so many cookies, and if you run out, you'll have to wait for your oven to bake more.</p>
 		<p>See how long you can last against the monsters (and try to get a High Score!) in this JavaScript-based game styled after classic arcade favorites.</p>
 		<p><strong>Skills</strong>: JavaScript, HTML, PHP, CSS, SQL/MariaDB</p>
 	</div>
+</div>
 
+<div class="project-div">
+	<div class="project-description">
+		<h1><a href="">Online Retailer: A CRUD App</a></h1>
+		<p>A basic CRUD web app simulating an online shopping experience. Written in C#/.NET and using an MVC design pattern.</p>
+		<p>PROJECT UNDER CONSTRUCTION (as of 11/29/2023)</p>
+		<p><strong>Skills</strong>: C#/.NET, MVC, Entity Framework, ORM, HTML/Razor, HTTP methods</p>
+	</div>
 	<div class="project-screenshot">
-		<a href="invaders.php"><img src="images/game-screenshot.png" style="float:right;"></a>
+		<a href=""><img src="images/construction.png" style="float:right;"></a>
 	</div>
 </div>
